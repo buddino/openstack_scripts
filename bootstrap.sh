866dd924ce969e10b3e224738fd85804a6810885
Giovanni Cuffaro <giovanni.cuffaro@cnit.it>
2017-11-22 11:10:37 +0100

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 sed -i 's/#net.ipv4.ip_forward=1/net.ipv4.ip_forward=1/g' /etc/sysctl.conf
 apt install git iperf3
-git clone http://multimedia.dinfo.unifi.it/git/budda/openstack_scripts.git
